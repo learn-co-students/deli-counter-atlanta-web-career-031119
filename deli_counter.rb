215f665284d0fe46bdf8aa2aa11f0fc02a0ac9b9
active-object-6703 <nerajno@gmail.com>
2019-02-04 00:44:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,17 @@
 # Write your code here.
 def line(katz_deli)
-
+ if katz_deli.length === 0
+   puts "The line is currently empty."
+elsif katz_deli.length > 0
+     puts katz_deli
+ end
 end
 
-def take_a_number()
+def take_a_number(katz_deli,person)
+   if katz_deli === 0
+     katz_deli.push(person)
 
+   end
 end
 
 def now_serving(katz_deli)
